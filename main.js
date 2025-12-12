cfb764c82057915d2f1efcfb14d285217705fddc
zhane249 <zhane249@newschool.edu>
2025-12-12 13:32:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("LIVE MAIN.JS --- HTTPS VERSION");
+console.log("LIVE MAIN.JS HTTPS VERSION LOADED");
 
 //the following code is credited with THREE.js Library and I modified code from Gabriel Molter youtube tutorial on 3D stuff https://www.youtube.com/watch?v=lGokKxJ8D2c&t=1s
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
