733ab7844934058c016b9b2f0aef3332dca5debb
zhane249 <zhane249@newschool.edu>
2026-02-05 09:40:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ loader.load(
 
 //camera from 3d model
            // camera.position.z = objToRender === "dino"? 25 : 50;
-camera.position.z = 120;
+camera.position.z = 70;
          //   controls = new OrbitControls(camera, renderer.domElement);
 
 //lights
