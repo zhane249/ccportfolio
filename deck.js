5bd6a042b7fb23ea9a36216a03d853c05a39f885
zhane249 <zhane249@newschool.edu>
2026-02-05 07:19:23 -0500

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,16 +1,44 @@
 // referenced from https://www.youtube.com/watch?v=w9OpXh3K6YI
 document.addEventListener("DOMContentLoaded", () => {
-  new Swiper(".swiper", {
+  let isDragging = false;
+
+  const swiper = new Swiper(".swiper", {
     effect: "cards",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 1,
     loop: true,
     cardsEffect: {
-      rotate: 0,    
-      perSlideOffset: 10, 
+      rotate: 0,
+      perSlideOffset: 10,
       perSlideRotate: 2,
-      slideShadows: false
+      slideShadows: false,
+    },
+    on: {
+      touchMove() {
+        isDragging = true;
+      },
+      touchEnd() {
+        setTimeout(() => {
+          isDragging = false;
+        }, 100);
+      },
+    },
+  });
+
+ 
+  document.querySelector(".swiper").addEventListener("click", () => {
+    if (isDragging) return;
+
+    const activeSlide = document.querySelector(".swiper-slide-active");
+    if (!activeSlide) return;
+
+    const card = activeSlide.querySelector(".card");
+    if (!card) return;
+
+    const link = card.dataset.link;
+    if (link) {
+      window.location.href = link;
     }
   });
 });
