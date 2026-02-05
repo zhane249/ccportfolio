3f88e1cbf6958f72fa9dd326b5fb1c306d0dc1df
zhane249 <zhane249@newschool.edu>
2026-02-05 07:07:25 -0500

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,20 +1,16 @@
-//the following code references the youtube tutorial https://www.youtube.com/watch?v=w9OpXh3K6YI
-  document.addEventListener("DOMContentLoaded", function () {
- const swiper = new Swiper(".swiper", {
- effect: "cards", grabCursor: true, centeredSlides: true,slidesPerView: "auto", loop: true,
-  breakpoints: {
-     640: {
-   slidesPerView: 1,
-    },
-    768: {
-  slidesPerView: 2,
-   spaceBetween: 20,
-   },
-   1024: {
-   slidesPerView: 3,
-              spaceBetween: 30,
-            },
-          },
-        });
-      });
-
+// referenced from https://www.youtube.com/watch?v=w9OpXh3K6YI
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    cardsEffect: {
+      rotate: 0,    
+      perSlideOffset: 10, 
+      perSlideRotate: 2,
+      slideShadows: false
+    }
+  });
+});
